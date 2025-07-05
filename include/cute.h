e12b1be52aa0000e991847b8b69c255b9ebca827
Fabrice <67000409+Fabbboy@users.noreply.github.com>
2025-07-05 14:06:09 +0200

    string: document api and refine result

--- include/cute.h
+++ include/cute.h
@@ -3,10 +3,9 @@
 #include "memory/allocator.h"
 #include "memory/page.h"
 
+#include "hash/hash.h"
+#include "macro.h"
 #include "object/optional.h"
 #include "object/result.h"
 #include "object/slice.h"
-
-#include "string/utf8.h"
-#include "hash/hash.h"
-#include "macro.h"
+#include "string/string.h"
