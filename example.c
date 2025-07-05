6bad16b2c6220d7d8768ea2cca9ccb9b0b23eebd
Fabrice <fabrice@schaub-dev.xyz>
2025-07-05 09:26:24 +0200

    adds utf8

--- example.c
+++ example.c
@@ -2,7 +2,6 @@
 #include "memory/allocator.h"
 #include "memory/page.h"
 #include "object/slice.h"
-#include "string/utf8.h"
 #include <stdalign.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,14 +19,6 @@ int main(void) {
 
   fprintf(stderr, "allocation successful!\n");
   cu_Allocator_Free(page_allocator, allocation.value);
-
-  const char *text = "hello👍";
-  bool valid = cu_utf8_validate((const unsigned char *)text, 5);
-  uint64_t hash = cu_Hash_FNV1a64(text, 5);
-  uint32_t cp = 0;
-  cu_utf8_decode((const unsigned char *)text, 5, &cp);
-  fprintf(stderr, "utf8 valid: %s, hash: %llu\n", valid ? "yes" : "no",
-      (unsigned long long)hash);
 
   exit(EXIT_SUCCESS);
 }
