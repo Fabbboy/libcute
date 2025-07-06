fe09ec368895dfed564d7664363d68f1f8b55525
Fabrice <fabrice@schaub-dev.xyz>
2025-07-06 08:10:53 +0200

    docs: designing the gpa

--- include/memory/gpallocator.h
+++ include/memory/gpallocator.h
@@ -1,14 +1,22 @@
 #pragma once
 
+#include "memory/allocator.h"
 #include "object/bitset.h"
-#define BUCKET_SIZE 4096
-#define NUM_SMALL_BUCKETS 16
-#define NUM_LARGE_BUCKETS 8
+
+#define CU_BUCKET_SIZE 4096
+#define CU_NUM_SMALL_BUCKETS                                                   \
+  16 // **< Number of small buckets, must be a power of 2 and cannot be
+     // overridden. */
+#define CU_NUM_LARGE_BUCKETS                                                   \
+  8 // **< Number of large buckets, must be a power of 2 and cannot be
+    // overridden. */
+#define CU_BUCKET_CANARY                                                       \
+  0x9232a6ff85dff10f // **< Canary value for bucket headers to detect
+                     // corruption. */
 
 struct cu_GPAllocator_BucketHeader;
 
-CU_BITSET_DECL(
-    cu_GPAllocator_BucketHeader, NUM_SMALL_BUCKETS + NUM_LARGE_BUCKETS)
+CU_BITSET_DECL(cu_GPAllocator_BucketHeader, CU_BUCKET_SIZE)
 CU_OPTIONAL_DECL(
     cu_GPAllocator_BucketHeaderPtr, struct cu_GPAllocator_BucketHeader *)
 
@@ -16,8 +24,17 @@ struct cu_GPAllocator_BucketHeader {
   cu_GPAllocator_BucketHeader_BitSet freeSlots;
   cu_GPAllocator_BucketHeaderPtr_Optional prevBucket;
   cu_GPAllocator_BucketHeaderPtr_Optional nextBucket;
+  size_t canary;
 };
 
-typedef struct {
+struct cu_GPAllocator_LargeAlloc {};
 
+typedef struct {
+  size_t
+      bucketSize; /**< Size of each bucket in bytes. Overrides BUCKET_SIZE. */
+} cu_GPAllocator_Config;
+
+typedef struct {
+  cu_Allocator backingAllocator;
+  struct cu_GPAllocator_BucketHeader *smallBuckets[CU_NUM_SMALL_BUCKETS];
 } cu_Allocator_GPAllocator;
