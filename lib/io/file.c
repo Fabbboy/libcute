09c5557e7b5cf4431a96648b166ace62d99c504f
Fabrice <fabrice@schaub-dev.xyz>
2025-07-26 07:42:59 +0200

    adds wasm32 target

--- lib/io/file.c
+++
@@ -1,7 +0,0 @@
-#include "io/file.h"
-#include "object/optional.h"
-#include "object/result.h"
-
-CU_OPTIONAL_IMPL(cu_Handle, cu_Handle)
-CU_OPTIONAL_IMPL(cu_File_CreateMode, cu_File_CreateMode)
-CU_RESULT_IMPL(cu_File, cu_File, cu_Io_Error)
