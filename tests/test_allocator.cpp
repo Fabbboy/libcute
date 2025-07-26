0255c693520f035d1d982d62435f4acd08796775
Fabrice <fabrice@schaub-dev.xyz>
2025-07-26 15:51:37 +0200

    fork2

--- tests/test_allocator.cpp
+++ tests/test_allocator.cpp
@@ -1,13 +1,20 @@
 #include <gtest/gtest.h>
 extern "C" {
 #include "memory/allocator.h"
+#include "memory/fixedallocator.h"
 #include "memory/gpallocator.h"
 }
 
+static char buffer[1024 * 1024];
+
 TEST(Allocator, GPABasic) {
+  cu_FixedAllocator fa;
+  cu_Allocator fa_alloc =
+      cu_Allocator_FixedAllocator(&fa, cu_Slice_create(buffer, sizeof(buffer)));
+
   cu_GPAllocator gpa;
   cu_GPAllocator_Config cfg = {0};
-  cfg.backingAllocator = cu_Allocator_Optional_none();
+  cfg.backingAllocator = cu_Allocator_Optional_some(fa_alloc);
   cfg.bucketSize = 64;
   cu_Allocator alloc = cu_Allocator_GPAllocator(&gpa, cfg);
 
