7cc7d3bf6256cee29128e0029281c93760cbd266
Fabrice <schaubfabrice@gmail.com>
2025-07-28 20:50:25 +0200

    fixes rc

--- include/object/rc.h
+++ include/object/rc.h
@@ -21,17 +21,16 @@
     CU_RC_DESTRUCTOR_NAME(NAME) destructor;                                    \
   };
 
-/** Result type for RC creation using existing result system */
+/** Declare functions for a reference counted type. */
 #define CU_RC_RESULT_NAME(NAME) NAME##_Rc_Result
 
-/** Declare functions for a reference counted type. */
 #define CU_RC_HEADER(NAME, T)                                                  \
   CU_RC_RESULT_NAME(NAME)                                                      \
-  CU_CONCAT(cu_, CU_RC_FN(NAME, _create))(                                     \
+  cu_##NAME##_Rc_create(                                                       \
       cu_Allocator alloc, T value, CU_RC_DESTRUCTOR_NAME(NAME) destructor);    \
-  CU_RC_NAME(NAME) CU_CONCAT(cu_, CU_RC_FN(NAME, _clone))(const CU_RC_NAME(NAME) * rc); \
-  void CU_CONCAT(cu_, CU_RC_FN(NAME, _destroy))(CU_RC_NAME(NAME) * rc);        \
-  T *CU_CONCAT(cu_, CU_RC_FN(NAME, _get))(const CU_RC_NAME(NAME) * rc);
+  CU_RC_NAME(NAME) cu_##NAME##_Rc_clone(const CU_RC_NAME(NAME) * rc);          \
+  void cu_##NAME##_Rc_destroy(CU_RC_NAME(NAME) * rc);                          \
+  T *cu_##NAME##_Rc_get(const CU_RC_NAME(NAME) * rc);
 
 /** Declare the rc struct and its helpers. */
 #define CU_RC_DECL(NAME, T)                                                    \
@@ -40,19 +39,20 @@
     struct CU_RC_FN(NAME, Container) * inner;                                  \
     cu_Allocator alloc;                                                        \
   } CU_RC_NAME(NAME);                                                          \
-  CU_RESULT_DECL(CU_RC_FN(NAME, Result), CU_RC_NAME(NAME), cu_IoError)        \
+  CU_RESULT_DECL(CU_RC_NAME(NAME), CU_RC_NAME(NAME), cu_Io_Error)              \
   CU_RC_HEADER(NAME, T)
 
 /** Define the implementation for the rc helpers. */
 #define CU_RC_IMPL(NAME, T)                                                    \
-  CU_RESULT_IMPL(CU_RC_FN(NAME, Result), CU_RC_NAME(NAME), cu_IoError)        \
+  CU_RESULT_IMPL(CU_RC_NAME(NAME), CU_RC_NAME(NAME), cu_Io_Error)              \
   CU_RC_RESULT_NAME(NAME)                                                      \
-  CU_RC_FN(NAME, _create)(                                                     \
+  cu_##NAME##_Rc_create(                                                       \
       cu_Allocator alloc, T value, CU_RC_DESTRUCTOR_NAME(NAME) destructor) {   \
-    cu_Slice_Result mem =                                                      \
-        cu_Allocator_Alloc(alloc, CU_LAYOUT(struct CU_RC_FN(NAME, Container))); \
+    cu_Slice_Result mem = cu_Allocator_Alloc(                                  \
+        alloc, CU_LAYOUT(struct CU_RC_FN(NAME, Container)));                   \
     if (!cu_Slice_Result_is_ok(&mem)) {                                        \
-      return CU_RC_FN(NAME, Result_error)(cu_Slice_Result_unwrap_error(&mem)); \
+      return CU_RESULT_FN(CU_RC_NAME(NAME), _error)(                           \
+          cu_Slice_Result_unwrap_error(&mem));                                 \
     }                                                                          \
     struct CU_RC_FN(NAME, Container) *cont =                                   \
         (struct CU_RC_FN(NAME, Container) *)cu_Slice_Result_unwrap(&mem).ptr;  \
@@ -60,9 +60,9 @@
     cont->ref_count = 1;                                                       \
     cont->destructor = destructor;                                             \
     CU_RC_NAME(NAME) rc = {cont, alloc};                                       \
-    return CU_RC_FN(NAME, Result_ok)(rc);                                      \
+    return CU_RESULT_FN(CU_RC_NAME(NAME), _ok)(rc);                            \
   }                                                                            \
-  CU_RC_NAME(NAME) CU_RC_FN(NAME, _clone)(const CU_RC_NAME(NAME) * rc) {       \
+  CU_RC_NAME(NAME) cu_##NAME##_Rc_clone(const CU_RC_NAME(NAME) * rc) {         \
     if (!rc || !rc->inner) {                                                   \
       CU_RC_NAME(NAME) tmp = {0};                                              \
       return tmp;                                                              \
@@ -71,7 +71,7 @@
     CU_RC_NAME(NAME) clone = {rc->inner, rc->alloc};                           \
     return clone;                                                              \
   }                                                                            \
-  void CU_RC_FN(NAME, _destroy)(CU_RC_NAME(NAME) * rc) {                       \
+  void cu_##NAME##_Rc_destroy(CU_RC_NAME(NAME) * rc) {                         \
     if (!rc || !rc->inner) {                                                   \
       return;                                                                  \
     }                                                                          \
@@ -84,7 +84,7 @@
     }                                                                          \
     rc->inner = NULL;                                                          \
   }                                                                            \
-  T *CU_RC_FN(NAME, _get)(const CU_RC_NAME(NAME) * rc) {                       \
+  T *cu_##NAME##_Rc_get(const CU_RC_NAME(NAME) * rc) {                         \
     CU_IF_NULL(rc) { return NULL; }                                            \
     CU_IF_NULL(rc->inner) { return NULL; }                                     \
     return &rc->inner->item;                                                   \
